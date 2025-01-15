85fbaa713c60417e374c95a26c3d69f14d1e93ff
tab1s4 <tabishghouri3@gmail.com>
2025-01-15 15:22:03 -0500

    cleaned up the data so that it can be used for predictions, no missing values

---
+++ cleaning_data.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+fighter_data = pd.read_csv('combined_fighter_data.csv', encoding='latin-1')
+
+# calculate medians
+median_height_by_weight = fighter_data.groupby('weight_pounds')['height_cm'].median().to_dict()
+median_weight_by_height = fighter_data.groupby('height_cm')['weight_pounds'].median().to_dict()
+median_reach_by_height = fighter_data.groupby('height_cm')['reach_cm'].median().to_dict()
+median_reach_by_weight = fighter_data.groupby('weight_pounds')['reach_cm'].median().to_dict()
+
+# function replace NA in height with median height based on weight
+def fill_height(row, median_height_by_weight):
+    if pd.isna(row['height_cm']):
+        return median_height_by_weight.get(row['weight_pounds'], row['height_cm'])
+    return row['height_cm']
+
+# function to replace NA in weight with median weight based on height
+def fill_weight(row, median_weight_by_height):
+    if pd.isna(row['weight_pounds']):
+        return median_weight_by_height.get(row['height_cm'], row['weight_pounds'])
+    return row['weight_pounds']
+
+# function to replace NA in reach with median reach based on height or weight
+def fill_reach(row, median_reach_by_height, median_reach_by_weight):
+    if pd.isna(row['reach_cm']):
+        if not pd.isna(row['height_cm']):
+            return median_reach_by_height.get(row['height_cm'], row['reach_cm'])
+        elif not pd.isna(row['weight_pounds']):
+            return median_reach_by_weight.get(row['weight_pounds'], row['reach_cm'])
+    return row['reach_cm']
+
+
+# apply the functions to fill missing values
+fighter_data['height_cm'] = fighter_data.apply(fill_height, axis=1, median_height_by_weight=median_height_by_weight)
+fighter_data['weight_pounds'] = fighter_data.apply(fill_weight, axis=1, median_weight_by_height=median_weight_by_height)
+fighter_data['reach_cm'] = fighter_data.apply(fill_reach, axis=1, median_reach_by_height=median_reach_by_height, median_reach_by_weight=median_reach_by_weight)
+
+# drop rows where both height and weight are NA
+fighter_data.dropna(subset=['height_cm', 'weight_pounds'], how='all', inplace=True)
+
+fighter_data.to_csv('cleaned_fighters.csv', index=False)
+
+
